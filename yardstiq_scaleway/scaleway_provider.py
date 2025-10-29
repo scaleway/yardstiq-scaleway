491d1846d26b5761bf4f83d1d2af977ca24b40f6
Valentin Macheret <vmacheret@scaleway.com>
2025-10-29 14:41:22 +0100

    feat(ys): continue wip

--- yardstiq_scaleway/scaleway_provider.py
+++ yardstiq_scaleway/scaleway_provider.py
@@ -1,8 +1,19 @@
 from typing import Any, Dict, List
 
 from yardstiq.core import provider, Provider, Backend
+from yardstiq.objects import BackendRunResult, ComputationModel
 
 from scaleway_qaas_client.v1alpha1 import QaaSClient, QaaSPlatform
+
+
+class ScalewayBackend(Backend):
+    def __init__(self, platform: QaaSPlatform, provider: "ScalewayProvider"):
+        super().__init__(provider=provider)
+
+        self.platform = platform
+
+    def run(model: ComputationModel) -> BackendRunResult:
+        pass
 
 
 @provider("scaleway")
@@ -14,9 +25,11 @@ class ScalewayProvider(Provider):
         project_id = kwargs.get("scaleway_project_id", None)
         api_url = kwargs.get("scaleway_api_url", None)
 
-        self.__client = QaaSClient(project_id=project_id, secret_key=secret_key, url=api_url)
+        self.__client = QaaSClient(
+            project_id=project_id, secret_key=secret_key, url=api_url
+        )
 
-    def get_backend(self, name : str) -> Backend:
+    def get_backend(self, name: str) -> ScalewayBackend:
         platforms = self.__client.list_platforms(name=name)
 
         if not platforms or len(platforms) == 0:
@@ -24,7 +37,7 @@ class ScalewayProvider(Provider):
 
         return self._platform_to_backend(self, platforms[0])
 
-    def list_backends(self) -> List[Backend]:
+    def list_backends(self) -> List[ScalewayBackend]:
         platforms = self.__client.list_platforms()
 
         if not platforms:
@@ -35,5 +48,5 @@ class ScalewayProvider(Provider):
 
         return list(map(lambda p: self._platform_to_backend(p), platforms))
 
-    def _platform_to_backend(self, platform: QaaSPlatform) -> Backend:
-        return Backend(provider=self)
+    def _platform_to_backend(self, platform: QaaSPlatform) -> ScalewayBackend:
+        return ScalewayBackend(provider=self)
